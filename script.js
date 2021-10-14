c4f4b9ed840c33bda600adf5f4268185244af339
Polina <p.artamonova@gmail.com>
2021-10-14 17:27:55 +0200

    cleaned up js

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ async function start() {
   let response = await fetch("empty_pot-01.svg");
   let mySvgData = await response.text();
   document.querySelector("div#svgDiv").innerHTML = mySvgData;
-  document.querySelectorAll(".option").forEach((option) => option.addEventListener("click", toggleOption));
+  document
+    .querySelectorAll(".option")
+    .forEach((option) => option.addEventListener("click", toggleOption));
   init();
 }
 
@@ -43,8 +45,12 @@ function init() {
   });
   document.querySelector("#colorInput").addEventListener("input", choseColor);
   document.querySelector("#refresh").addEventListener("click", refreshPlant);
-  document.querySelector("#refreshAddon").addEventListener("click", refreshAddons);
-  document.querySelector("#refresh_button").addEventListener("click", refreshColor);
+  document
+    .querySelector("#refreshAddon")
+    .addEventListener("click", refreshAddons);
+  document
+    .querySelector("#refresh_button")
+    .addEventListener("click", refreshColor);
 }
 
 function choseColor(event) {
@@ -72,40 +78,34 @@ function toggleOption(event) {
   const feature = target.dataset.feature;
   const sort = target.dataset.sort;
   const previewToAnimate = document.querySelector(`[data-feature=${feature}]`);
-  //  console.log(sort);
   if (features[feature] == false) {
     // feature added
     features[feature] = true;
-    // TODO: More code
-    // console.log(`Feature ${feature} is turned on!`);
     previewToAnimate.classList.add("chosen");
     previewToAnimate.classList.remove("hide");
     previewToAnimate.style.left = "35%";
     swipeToCenter(feature);
-    // createImg(feature);
-
-    // }
-
-    // TODO: More code
-  } else {
+  } else if (features[feature] == true) {
     // feature removed
     features[feature] = false;
 
-    //   console.log(`Feature ${feature} is turned off!`);
-    previewToAnimate.classList.remove("chosen");
-    // previewToAnimate.classList.add("hide");
+    // previewToAnimate.removeAttribute("class");
+    previewToAnimate.classList.add("hide");
     // previewToAnimate.style.left = "0%";
-    swipeToEnd(feature);
+    // swipeToEnd(feature);
   }
-  document.querySelectorAll(`#preview [data-sort=${sort}]`).forEach((displayed_feature) => {
-    let currentlyChosen = document.querySelector(".chosen");
-    //   console.log(currentlyChosen);
-    // swipeToEnd(displayed_feature.dataset.feature);
-    console.log("displayed feature is", displayed_feature.dataset.feature);
-    displayed_feature.classList.add("hide");
-    console.log(`all ${sort} is removed`);
-  });
-  let currentlyChosen = document.querySelector(".chosen");
+  document
+    .querySelectorAll(`#preview [data-sort=${sort}]`)
+    .forEach((displayed_feature) => {
+      // let currentlyChosen = document.querySelector(".chosen");
+      //   console.log(currentlyChosen);
+      // swipeToEnd(displayed_feature.dataset.feature);
+      // console.log("displayed feature is", displayed_feature.dataset.feature);
+      // displayed_feature.classList.remove("chosen");
+      displayed_feature.classList.add("hide");
+      // console.log(`all ${sort} is removed`);
+    });
+  // let currentlyChosen = document.querySelector(".chosen");
   // console.log(currentlyChosen);
   previewToAnimate.classList.remove("hide");
 }
@@ -113,11 +113,10 @@ function toggleOption(event) {
 function swipeToCenter(feature) {
   const previewToAnimate = document.querySelector(`[data-feature=${feature}]`);
 
-  const destination = document.querySelector("#svgDiv svg").getBoundingClientRect();
+  const destination = document
+    .querySelector("#svgDiv svg")
+    .getBoundingClientRect();
   console.log("last frame", destination);
-  // setToFalse(previewToAnimate.dataset.feature);
-  // const deltaX = firstFrame.right - lastFrame.left;
-  // console.log(deltaX);
 
   previewToAnimate.animate(
     //KEYFRAMES
@@ -143,76 +142,24 @@ function swipeToCenter(feature) {
   );
 }
 
-function swipeToEnd(feature) {
-  // console.log("removing previous");
-  console.log(feature);
-
-  document.querySelector(`[data-feature=${feature}]`).classList.add("hide");
-  // console.log(previewToAnimate);
-
-  // const firstFrame = document
-  //   .querySelector("#svgDiv svg")
-  //   .getBoundingClientRect();
-
-  // const destination = document
-  //   .querySelector("#svgDiv svg")
-  //   .getBoundingClientRect();
-  // console.log("last frame", destination);
-
-  // previewToAnimate.animate(
-  //KEYFRAMES
-
-  //   [
-  //     {
-  //       transformOrigin: "center",
-  //       transform: "none",
-  //     },
-  //     {
-  //       transformOrigin: "center",
-  //       transform: `translateX(-${destination.width / 2}px)`,
-  //     },
-  //   ],
-  //   //PROPERTIES
-
-  //   {
-  //     duration: 1000,
-  //     easing: "ease-in-out",
-  //     iterations: "1",
-  //     fill: "forwards",
-  //   }
-  // );
-  // previewToAnimate.addEventListener("animationend", function () {
-  //   console.log("hide");
-  // });
-}
-
-function setToFalse(feature) {
-  features[feature] = false;
-}
-// function createImg(feature) {
-//   const selectedFeatureImg = document.createElement("img");
-//   selectedFeatureImg.id = `${feature}Created`;
-//   selectedFeatureImg.src = `assets/${feature}.png`;
-//   selectedFeatureImg.alt = capitalize(feature);
-//   selectedFeatureImg.style.height = "8vw";
-//   document.querySelector("#selected ul").appendChild(selectedFeatureImg);
-// }
-// function capitalize(text) {
-//   return text.substring(0, 1).toUpperCase() + text.substring(1).toLowerCase();
-// }
-
 function refreshPlant() {
-  document.querySelectorAll(`#preview [data-sort="plants"]`).forEach((element) => {
-    element.classList.remove("chosen");
-    element.classList.add("hide");
-  });
+  document
+    .querySelectorAll(`#preview [data-sort="plants"]`)
+    .forEach((element) => {
+      element.classList.remove("chosen");
+      element.classList.add("hide");
+      features[element.dataset.feature] = false;
+    });
 }
 
 function refreshAddons() {
-  document.querySelectorAll(`#preview [data-sort="addOn"]`).forEach((element) => {
-    element.classList.remove("chosen");
-    element.classList.add("hide");
-  });
+  document
+    .querySelectorAll(`#preview [data-sort="addOn"]`)
+    .forEach((element) => {
+      element.classList.remove("chosen");
+      element.classList.add("hide");
+      features[element.dataset.feature] = false;
+    });
 }
 
 function refreshColor() {
